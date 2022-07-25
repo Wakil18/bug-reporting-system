2d7e8636d542d75ac50226530df1e5bf08ae3554
Wakil18 <wakilahmed18@gmail.com>
2022-07-25 09:50:06 +0600

    Basic setup -- Added basic methods in App and Config Class

---
+++ Src/Helpers/App.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Helpers;
+
+use PHPUnit\Util\Exception;
+use DateTimeInterface, DateTime, DateTimeZone;
+
+class App
+{
+    private $config = [];
+
+    public function __construct()
+    {
+        $this->config = config::get('app');
+    }
+
+    public function isDebugMode(): bool
+    {
+        if (!isset($this->config['debug'])){
+            return false;
+        }
+        return $this->config['debug'];
+    }
+
+    public function getEnvironment(): string
+    {
+        if (!isset($this->config['env'])){
+            return 'production';
+        }
+        return $this->config['env'];
+    }
+
+    public function getLogPath(): string
+    {
+        if (!isset($this->config['log_path'])){
+            throw new Exception('Log path is not defined');
+        }
+        return $this->config['log_path'];
+    }
+
+    /**
+     * To see if php is running in the command line.
+     **/
+    public function isRunningFromConsole(): bool
+    {
+        return php_sapi_name() == 'cli' || php_sapi_name() == 'phpbg';
+    }
+
+    /**
+     * To get the server time.
+     **/
+    public function getServerTime(): \DateTimeInterface
+    {
+        return new DateTime('now', new DateTimeZone('Asia/Dhaka'));
+    }
+}
--- index.php
+++ index.php
@@ -3,5 +3,11 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$app = new \App\Helpers\App();
+echo $app->getServerTime()->format('y-m-d H:i:s') .PHP_EOL;
+echo $app->getLogPath() .PHP_EOL;
+echo $app->getEnvironment() .PHP_EOL;
+echo $app->isDebugMode() .PHP_EOL;
+echo $app->isRunningFromConsole() .PHP_EOL;
 
 
